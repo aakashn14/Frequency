f502838badadd15d50f4f267c4c6b514a2b09a12
ENathanLe <lee.nathan8621@gmail.com>
2024-03-13 04:19:21 -0700

    Remove change jest config, add collect coverage, add to .gitignore

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,16 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
   ],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy',
-  },
   "transform": {
       "^.+\\.js$": "babel-jest",
       "^.+\\.jsx$": "babel-jest"
-  }
+  },
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "**/*.{js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js"
+  ]
 };
